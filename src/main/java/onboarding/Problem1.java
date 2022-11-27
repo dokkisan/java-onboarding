924a79df9832c5b8d5436064a51020fa1695bad4
hanawang <dokkisan.wang@gmail.com>
2022-11-28 05:23:46 +0900

    feat: setup precourse onboarding project

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -1,10 +1,52 @@
 package onboarding;
 
+import java.util.Arrays;
 import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
         int answer = Integer.MAX_VALUE;
+
+        if (checkError(pobi) || checkError(crong)) {
+            return -1;
+        }
+
+        int pobiResult = findMaxNum(pobi);
+        int crongResult = findMaxNum(crong);
+
+        if (pobiResult > crongResult) {
+            answer = 1;
+        } else if (pobiResult < crongResult) {
+            answer = 2;
+        } else {
+            answer = 0;
+        }
+
         return answer;
+    }
+
+    public static int findMaxNum(List<Integer> input) {
+        int maxNum = 0;
+
+        for (int i = 0; i < input.size(); i++) {
+            String[] num = String.valueOf(input.get(i)).split("");
+            int sum = 0;
+            int mutiply = 1;
+            for (int j = 0; j < num.length; j++) {
+                sum += Integer.parseInt(num[j]);
+                mutiply *= Integer.parseInt(num[j]);
+            }
+
+            maxNum = Math.max(Math.max(sum, mutiply), maxNum);
+        }
+
+        return maxNum;
+    }
+
+    public static boolean checkError(List<Integer> input) {
+        if ((input.get(0) + 1) == input.get(1)) {
+            return false;
+        }
+        return true;
     }
 }
